96e7f722be48da050933d6dbc587900b02812f6b
Code Kie <codekie@users.noreply.github.com>
2018-11-08 16:37:54 +0100

    test: add more tests for the task-component

--- src/client/component/task/task.spec.jsx
+++ src/client/component/task/task.spec.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Task from './task.jsx';
 
+const TIMESTAMP__DUE = 1541687666487;
+
 it('renders correctly', () => {
+    let _origNow = Date.now;
+
+    beforeEach(() => {
+        Date.now = jest.fn(() => TIMESTAMP__DUE + 1000);
+    });
+    afterEach(() => {
+        Date.now = _origNow;
+    });
+
     expect(shallow(
         <Task deactivateTask={jest.fn()} activateTask={jest.fn()} finishTask={jest.fn()} id={1}
-            unfinishTask={jest.fn()} uuid="ABC" />)
+            unfinishTask={jest.fn()} uuid="ABC" project="spartaaa" due={TIMESTAMP__DUE} />)
     ).toMatchSnapshot();
 });
