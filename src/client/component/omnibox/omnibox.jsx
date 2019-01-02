2a2f88de1adc95d1154664c3f54dd823a12db826
Code Kie <codekie@users.noreply.github.com>
2019-01-02 01:27:35 +0100

    chore: render tags and project into omnibox

--- src/client/component/omnibox/omnibox.jsx
+++ src/client/component/omnibox/omnibox.jsx
@@ -1,7 +1,15 @@
 import React, { PureComponent } from 'react';
 import './omnibox.scss';
 import PropTypes from 'prop-types';
+import Panel from 'react-bulma-components/lib/components/panel';
 import { Input } from 'react-bulma-components/lib/components/form';
+import Tag from 'react-bulma-components/lib/components/tag';
+import Icon from 'react-bulma-components/lib/components/icon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFolder } from '@fortawesome/free-solid-svg-icons';
+
+const COLOR__TAG__PROJECT = 'info',
+    COLOR__TAG = 'primary';
 
 export default class Omnibox extends PureComponent {
     static propTypes = {
@@ -11,7 +19,8 @@ export default class Omnibox extends PureComponent {
             tags: PropTypes.arrayOf(PropTypes.string),
             priority: PropTypes.string,
             project: PropTypes.string,
-            status: PropTypes.string
+            status: PropTypes.string,
+            searchTerm: PropTypes.string
         }),
         rawValue: PropTypes.string,
         setRawValue: PropTypes.func
@@ -33,11 +42,39 @@ export default class Omnibox extends PureComponent {
         }
     }
     render() {
-        const { rawValue, setRawValue } = this.props;
+        const { rawValue, setRawValue, parsedValues } = this.props,
+            tags = parsedValues.tags;
         return (
-            <Input size="small" type="text" className="cmp-sub-input" placeholder="search" value={rawValue}
-                onChange={(event) => setRawValue(event.target.value)}
-                onKeyDown={(event) => this.handleKeyDown(event)} />
+            <Panel className="cmp-omnibox">
+                { _createProjectItem(parsedValues.project) }
+                {
+                    tags.length > 0 && (
+                        <Tag.Group className="cmp-sub-tags">{
+                            tags.map((tag) => (
+                                <Tag key={tag} color={COLOR__TAG} tabIndex={0}>{ tag }</Tag>
+                            ))
+                        }</Tag.Group>
+                    )
+                }
+                <Input size="small" type="text" className="cmp-sub-input" placeholder="search" value={rawValue}
+                    onChange={(event) => setRawValue(event.target.value)}
+                    onKeyDown={(event) => this.handleKeyDown(event)} />
+            </Panel>
         );
     }
 }
+
+function _createProjectItem(project) {
+    if (!project) { return null; }
+    return (
+        <Tag.Group gapless className="tag-project tag-icon" tabIndex={0}>
+            <Tag color="dark">
+                <Icon className="is-small">
+                    <FontAwesomeIcon icon={faFolder} />
+                </Icon>
+            </Tag>
+            <Tag color={COLOR__TAG__PROJECT}>{ project }</Tag>
+        </Tag.Group>
+    );
+}
+
